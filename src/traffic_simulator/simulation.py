001c2a1bb3ad26dea770938f6eb5474d5fc3398f
Samurai-Human-Go_Jetson <gotechsamurai@samurai-human-go.com>
2025-10-02 09:12:00 +0900

    fix: 交差点を通過した車両が停止しないようにする,信号サイクルの同期

--- src/traffic_simulator/simulation.py
+++ src/traffic_simulator/simulation.py
@@ -1,8 +1,8 @@
-# simulation.py
 import random
 
 class TrafficLight:
     """信号機の状態を管理するクラス"""
+    # 修正点: __init__メソッドに引数 initial_state と durations を追加
     def __init__(self, initial_state, durations):
         self.states = ["青", "黄", "赤"]
         self.durations = durations
@@ -51,14 +51,17 @@ class Vehicle:
 class Simulation:
     """シミュレーション全体を管理するクラス"""
     def __init__(self):
-        # 南北: 青15s, 黄3s, 赤22s
+        # 1サイクルの合計時間を40秒に統一する
+        
+        # 南北: 青(15s) + 黄(3s) + 赤(22s) = 40s
         self.ns_light = TrafficLight("青", {"青": 15, "黄": 3, "赤": 22})
-        # 東西: 赤18s, 青10s, 黄2s
-        self.we_light = TrafficLight("赤", {"赤": 18, "青": 10, "黄": 2})
+        
+        # 東西: 青(10s) + 黄(2s) + 赤(28s) = 40s
+        self.we_light = TrafficLight("赤", {"赤": 28, "青": 10, "黄": 2})
         
         self.vehicles = []
         self.time_since_last_spawn = 0
-        self.spawn_interval = 2 # 2秒ごとに車両を生成
+        self.spawn_interval = 2
 
     def update(self, delta_time):
         """シミュレーションの状態を1フレーム分更新する"""
@@ -89,45 +92,51 @@ class Simulation:
         direction = random.choice([1, -1])
         self.vehicles.append(Vehicle(road, direction))
 
-# simulation.py の check_can_move メソッドを書き換え
     def check_can_move(self, vehicle):
         """車両が信号に従って動けるか判定する"""
-        # 交差点の境界座標
-        INTERSECTION_Y_START = 350
-        INTERSECTION_Y_END = 550
-        INTERSECTION_X_START = 350
-        INTERSECTION_X_END = 550
-
-        # 車両の先端が停止すべきかを判定する範囲
-        DETECTION_RANGE = 50 
+        # --- 停止線の座標を定義 ---
+        # 北から南へ向かう車両の停止線 (Y座標)
+        STOP_LINE_N = 350
+        # 南から北へ向かう車両の停止線 (Y座標)
+        STOP_LINE_S = 550
+        # 西から東へ向かう車両の停止線 (X座標)
+        STOP_LINE_W = 350
+        # 東から西へ向かう車両の停止線 (X座標)
+        STOP_LINE_E = 550
 
         if vehicle.road == "NS":
             light_state = self.ns_light.current_state
+            # 信号が赤または黄の場合のみ、停止判定を行う
             if light_state in ["赤", "黄"]:
-                # 北から南へ向かう車両 (direction=-1)
-                # 車両の下端が交差点の北端に近づいているか
-                vehicle_front_y = vehicle.y + vehicle.height
-                if (INTERSECTION_Y_START - DETECTION_RANGE) < vehicle_front_y < INTERSECTION_Y_START:
-                    return False
-            
-                # 南から北へ向かう車両 (direction=1)
-                # 車両の上端が交差点の南端に近づいているか
-                vehicle_front_y = vehicle.y
-                if INTERSECTION_Y_END < vehicle_front_y < (INTERSECTION_Y_END + DETECTION_RANGE):
-                    return False
+                # 北から南へ (direction=-1)。停止線に到達する前か？
+                if vehicle.direction == -1:
+                    vehicle_front_y = vehicle.y + vehicle.height
+                    # 停止線の手前50ピクセルの範囲に先端が入ったら停止
+                    if (STOP_LINE_N - 50) < vehicle_front_y < STOP_LINE_N:
+                        return False
+                
+                # 南から北へ (direction=1)。停止線に到達する前か？
+                elif vehicle.direction == 1:
+                    vehicle_front_y = vehicle.y
+                    # 停止線の手前50ピクセルの範囲に先端が入ったら停止
+                    if STOP_LINE_S < vehicle_front_y < (STOP_LINE_S + 50):
+                        return False
         else: # WE
             light_state = self.we_light.current_state
             if light_state in ["赤", "黄"]:
-                # 東から西へ向かう車両 (direction=-1)
-                # 車両の右端が交差点の東端に近づいているか
-                vehicle_front_x = vehicle.x + vehicle.height # 横向きなのでheight
-                if INTERSECTION_X_END < vehicle_front_x < (INTERSECTION_X_END + DETECTION_RANGE):
-                    return False
+                # 西から東へ (direction=1)。停止線に到達する前か？
+                if vehicle.direction == 1:
+                    vehicle_front_x = vehicle.x
+                    # 停止線の手前50ピクセルの範囲に先端が入ったら停止
+                    if (STOP_LINE_W - 50) < vehicle_front_x < STOP_LINE_W:
+                        return False
 
-                # 西から東へ向かう車両 (direction=1)
-                # 車両の左端が交差点の西端に近づいているか
-                vehicle_front_x = vehicle.x
-                if (INTERSECTION_X_START - DETECTION_RANGE) < vehicle_front_x < INTERSECTION_X_START:
-                    return False
-        
+                # 東から西へ (direction=-1)。停止線に到達する前か？
+                elif vehicle.direction == -1:
+                    vehicle_front_x = vehicle.x + vehicle.height # 横向きなのでheightが幅
+                    # 停止線の手前50ピクセルの範囲に先端が入ったら停止
+                    if STOP_LINE_E < vehicle_front_x < (STOP_LINE_E + 50):
+                        return False
+
+        # 上記の停止条件に当てはまらない場合は、すべて動ける
         return True
